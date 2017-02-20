338150115afa00b031dcb2e2729551712536fdd6
Tallis Hobbs <rhtallis@gmail.com>
2017-02-20 14:10:19 -0800

    Backend start

---
+++ jwt_part4/jwt/Controllers/AccountsController.cs
@@ -0,0 +1,153 @@
+﻿using jwt.Infrastructure;
+using jwt.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace jwt.Controllers {
+[RoutePrefix("api/accounts")]
+public class AccountsController : BaseApiController {
+    [Authorize(Roles="Admin")] 
+	[Route("users")]
+	public IHttpActionResult GetUsers() {
+		return Ok(this.AppUserManager.Users.ToList()
+                 .Select(u => this.TheModelFactory.Create(u)));
+	}
+
+    [Authorize(Roles="Admin")] 
+	[Route("user/{id:guid}", Name = "GetUserById")]
+	public async Task<IHttpActionResult> GetUser(string Id) {
+		var user = await this.AppUserManager.FindByIdAsync(Id);
+		if (user != null) {
+			return Ok(this.TheModelFactory.Create(user));
+		}
+		return NotFound();
+	}
+
+    [Authorize(Roles="Admin")] 
+	[Route("user/{username}")]
+	public async Task<IHttpActionResult> GetUserByName(string username) {
+		var user = await this.AppUserManager.FindByNameAsync(username);
+
+		if (user != null)
+		{
+			return Ok(this.TheModelFactory.Create(user));
+		}
+		return NotFound();
+	}
+
+    [Route("create")]
+    public async Task<IHttpActionResult> CreateUser(
+                 CreateUserBindingModel createUserModel) {
+	    if (!ModelState.IsValid) {
+		    return BadRequest(ModelState);
+	    }
+        var existingUser 
+        = await this.AppUserManager
+        .FindByEmailAsync(createUserModel.Email.ToLower());
+
+        if(existingUser!= null) {
+            return BadRequest(ModelState);
+        }
+	    var user = new ApplicationUser() {
+		    UserName = createUserModel.Username,
+		    Email = createUserModel.Email,
+		    FirstName = createUserModel.FirstName,
+		    LastName = createUserModel.LastName,
+		    Level = 3,
+		    JoinDate = DateTime.Now.Date,
+	    };
+
+	    IdentityResult addUserResult = 
+        await this.AppUserManager.CreateAsync(user, createUserModel.Password);
+
+	    if (!addUserResult.Succeeded) {
+		    return GetErrorResult(addUserResult);
+	    }
+
+        string code = await this.AppUserManager.GenerateEmailConfirmationTokenAsync(user.Id);
+
+        var callbackUrl = new Uri(Url.Link("ConfirmEmailRoute", 
+                          new { userId = user.Id, code = code }));
+
+        await this.AppUserManager.SendEmailAsync(user.Id,"Confirm your account", 
+           "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
+
+        Uri locationHeader = new Uri(Url.Link("GetUserById", new { id = user.Id }));
+        return Created(locationHeader, TheModelFactory.Create(user));
+
+    }
+
+    [Authorize(Roles="Admin")]
+    [Route("user/{id:guid}/roles")]
+    [HttpPut]
+    public async Task<IHttpActionResult> AssignRolesToUser([FromUri] string id,
+        [FromBody] string[] rolesToAssign) {
+	    var appUser = await this.AppUserManager.FindByIdAsync(id);
+	    if (appUser == null) {
+		    return NotFound();
+	    }
+	    var currentRoles   = await this.AppUserManager.GetRolesAsync(appUser.Id);
+	    var rolesNotExists = rolesToAssign.Except(this.AppRoleManager
+            .Roles.Select(x => x.Name)).ToArray();
+
+	    if (rolesNotExists.Count() > 0) {
+		    ModelState.AddModelError("", 
+                string.Format("Roles '{0}' does not exixts in the system", 
+                string.Join(",", rolesNotExists)));
+		    return BadRequest(ModelState);
+	    }
+
+	    IdentityResult removeResult = await 
+            this.AppUserManager.RemoveFromRolesAsync(appUser.Id, currentRoles.ToArray());
+	    if (!removeResult.Succeeded) {
+		    ModelState.AddModelError("", "Failed to remove user roles");
+		    return BadRequest(ModelState);
+	    }
+
+	    IdentityResult addResult 
+        = await this.AppUserManager.AddToRolesAsync(appUser.Id, rolesToAssign);
+
+	    if (!addResult.Succeeded) {
+		    ModelState.AddModelError("", "Failed to add user roles");
+		    return BadRequest(ModelState);
+	    }
+	    return Ok();
+    }
+
+    [Authorize]
+    [Route("ChangePassword")]
+    public async Task<IHttpActionResult> ChangePassword(ChangePasswordBindingModel model) {
+        if (!ModelState.IsValid) {
+            return BadRequest(ModelState);
+        }
+
+        IdentityResult result = await this.AppUserManager
+       .ChangePasswordAsync(User.Identity.GetUserId(), model.OldPassword, model.NewPassword);
+
+        if (!result.Succeeded) {
+            return GetErrorResult(result);
+        }
+        return Ok();
+    }
+
+    [HttpGet]
+    [Route("ConfirmEmail", Name = "ConfirmEmailRoute")]
+    public async Task<IHttpActionResult> ConfirmEmail(string userId = "", string code = "") {
+        if (string.IsNullOrWhiteSpace(userId) || string.IsNullOrWhiteSpace(code)) {
+            ModelState.AddModelError("", "User Id and Code are required");
+            return BadRequest(ModelState);
+        }
+
+        IdentityResult result = await this.AppUserManager.ConfirmEmailAsync(userId, code);
+        if (result.Succeeded) {
+            return Ok();
+        }
+        else {
+            return GetErrorResult(result);
+        }
+    }
+}
+}
---
+++ jwt_part4/jwt/Services/EmailService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace jwt.Services {
+public class EmailService : IIdentityMessageService {
+    public async Task SendAsync(IdentityMessage message) {
+        await configSendGridasync(message);
+    }
+    // Use NuGet to install SendGrid (Basic C# client lib) 
+    private async Task configSendGridasync(IdentityMessage message) {
+    }
+}
+}
